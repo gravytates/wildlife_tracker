b525ffd28f292c90033f42efae25442e4f7048d7
Grady Shelton and Sara Schultz <grady+sara@>
2017-06-26 09:51:21 -0700

    add routes for all animals and for specific animal

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,11 @@
+class AnimalsController < ApplicationController
+  def index
+    @animals = Animal.all
+    render :index
+  end
+
+  def show
+    @animal = Animal.find(params[:id])
+    render :show
+  end
+end
