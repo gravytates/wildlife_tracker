46c0e5f3ed5fab12d9a7c376ba3095f8f671fa23
Grady Shelton and Sara Schultz <grady@>
2017-06-26 14:39:13 -0700

    pass spec for updating a sighting

---
+++ spec/features/edit_sighting_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'the edit a sighting process' do
+  it 'edits a sighting' do
+    animal = Animal.create(:species => 'Squirrel', :name => 'Sara', :id => 1)
+    sighting = Sighting.create(:description => 'playing tricks and snapping necks', :date => '26-06-17', :lat => '666.666', :long => '666.666', :region => 'Arctic', :animal_id => 1)
+    visit animal_path(animal)
+    click_on 'Edit playing tricks and snapping necks'
+    fill_in 'Region', :with => 'Desert'
+    click_on 'Update Sighting'
+    expect(page).to have_content 'playing tricks and snapping necks'
+  end
+end
