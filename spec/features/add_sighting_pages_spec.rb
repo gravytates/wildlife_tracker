0386ebca80e39e9a290264fee99426476157fd8a
Grady Shelton and Sara Schultz <grady@>
2017-06-26 14:04:34 -0700

    pass spec checking for errors in new sightings

--- spec/features/add_sighting_pages_spec.rb
+++ spec/features/add_sighting_pages_spec.rb
@@ -13,4 +13,17 @@ describe 'the adding sighting process' do
     click_on 'Create Sighting'
     expect(page).to have_content 'Add sighting'
   end
+
+  it 'produces errors' do
+    animal = Animal.create(:species => 'Squirrel', :name => 'Sara')
+    visit animal_path(animal)
+    click_on 'Add sighting'
+    fill_in 'Description', :with => 'playing tricks and snapping necks'
+    fill_in 'Date', :with => '26-06-17'
+    fill_in 'Lat', :with => '666.666'
+    fill_in 'Long', :with => ''
+    fill_in 'Region', :with => 'Arctic'
+    click_on 'Create Sighting'
+    expect(page).to have_content 'errors'
+  end
 end
