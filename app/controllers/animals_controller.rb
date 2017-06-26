249a6194092dcf70511ffed63a2ef86190d92efb
Grady Shelton and Sara Schultz <grady@>
2017-06-26 10:29:28 -0700

    add new and edit funcitonality

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,11 +1,41 @@
 class AnimalsController < ApplicationController
   def index
     @animals = Animal.all
-    render :index
   end
 
   def show
     @animal = Animal.find(params[:id])
-    render :show
   end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      redirect_to animals_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    if @animal.update(animal_params)
+      redirect_to animals_path
+    else
+      render :edit
+    end
+  end
+
+private
+  def animal_params
+    params.require(:animal).permit(:species, :date, :lat, :long, :region)
+  end
+
 end
