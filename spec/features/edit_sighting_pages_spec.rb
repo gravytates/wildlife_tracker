27ea085813f1e37b2304c5ae00c99cdcfef451a8
Grady Shelton and Sara Schultz <grady@>
2017-06-26 14:40:11 -0700

    pass spec for errors when updating a sighting

--- spec/features/edit_sighting_pages_spec.rb
+++ spec/features/edit_sighting_pages_spec.rb
@@ -10,4 +10,14 @@ describe 'the edit a sighting process' do
     click_on 'Update Sighting'
     expect(page).to have_content 'playing tricks and snapping necks'
   end
+
+  it 'gives error when a field is left blank' do
+    animal = Animal.create(:species => 'Squirrel', :name => 'Sara', :id => 1)
+    sighting = Sighting.create(:description => 'playing tricks and snapping necks', :date => '26-06-17', :lat => '666.666', :long => '666.666', :region => 'Arctic', :animal_id => 1)
+    visit animal_path(animal)
+    click_on 'Edit playing tricks and snapping necks'
+    fill_in 'Region', :with => ''
+    click_on 'Update Sighting'
+    expect(page).to have_content 'errors'
+  end
 end
